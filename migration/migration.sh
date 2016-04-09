be0248cbccb468f166eeefa3026eda3d209ae5af
Hector Paz <hector.paz@clicksandbricks.pe>
2016-04-09 01:01:11 -0500

    migration

--- migration/migration.sh
+++ migration/migration.sh
@@ -4,9 +4,12 @@ count=0
 for container in `cat $1`
 do
    count=$((count+1))
-   echo  "[$count/$total] - $container"
    container_image=${container:0:32}
+   echo  "[$count/$total] - $container - $container_image"
    docker commit $container backup/$container_image
-   docker tag backup/$container_image registry.codepicnic.com:5000/backup/$container_image
+   docker tag -f backup/$container_image registry.codepicnic.com:5000/backup/$container_image
    docker push registry.codepicnic.com:5000/backup/$container_image
+   docker rmi backup/$container_image
+   #break
 done
+
