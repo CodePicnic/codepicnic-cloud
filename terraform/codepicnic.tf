978c32d06092a31fbbf86cafa409a61454d0bec7
Hector Paz <hector.paz@clicksandbricks.pe>
2016-10-10 17:10:04 -0500

    terraform

--- terraform/codepicnic.tf
+++ terraform/codepicnic.tf
@@ -5,7 +5,12 @@ provider "aws" {
 resource "aws_instance" "aws-devpad-swarm-master" {
   ami           = "ami-2d39803a"
   instance_type = "t2.micro"
-  security_groups = ["sg-33871c4b"]
+  vpc_security_group_ids = ["sg-33871c4b"]
   subnet_id = "subnet-5a9d2e70"
   key_name = "key_swarm"
+}
+
+resource "aws_eip" "aws-devpad-ip-001" {
+    instance = "${aws_instance.aws-devpad-swarm-master.id}"
+    vpc = true
 }
